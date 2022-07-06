2e88afa5cfc547e40a68374809f521d9b917373f
facert <facert@gmail.com>
2022-07-06 16:56:38 +0800

    feat: add setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="web3-security-rule-engine",
+    version="1.0",
+    author="Debank",
+    author_email="0xjeffer@gmail.com",
+    description="Web3 Security Rule Engine",
+    url="web3 security rule engine", 
+    packages=find_packages(),
+    python_requires='>=3.7',
+    install_requires=[
+        'gitdb==4.0.9',
+        'GitPython==3.1.27',
+        'PyYAML==6.0',
+        'smmap==5.0.0'
+    ]
+)
